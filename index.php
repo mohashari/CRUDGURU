6627ec50953d788f7641657955b5334d16fd955d
Moh Ashari Muklis <muklis@nostratech.com>
2017-12-02 13:27:53 +0700

    new php

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>CRUD SEKOLAH</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    </head>
+<body>
+<div class="container">
+    <div class="jumbotron">
+    <h1>Aplikasi Crud Guru</h1>
+</div>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">CRUD</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active">
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="input.php">INPUT</a>
+            </li>
+            <li>
+                <a href="data.php">DATA</a>
+            </li>
+            
+        </ul>
+    </div>
+</nav>
+</div>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+$id="root";
+$pass="welcome1";
+$db="db_sekolah";
+
+$conn= mysqli_connect($id,$pass,$db);
+
+?>
